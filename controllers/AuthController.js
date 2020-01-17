47740470cdfb8b2129d92b73af657c6059c8793c
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2020-01-17 21:12:46 +0200

    Install NodeMailer Package.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,53 +1,53 @@
 const user = require('../models/User');
 
 const homePage = (req, res, next) => {
-  res.send('<h1>HELLO FROM USER CONTROLLER</h1>');
+    res.send('<h1>HELLO FROM USER CONTROLLER</h1>');
 };
 
 // USER FUNCTIONS ****************************************************************************
 
 // Get one user.
 const login = (req, res, next) => {
-  user.getUser(req.body, result => {
-    console.log('RESULT FROM LOGIN RESPONSE: ', result);
-    // res.json(result);
-    res.status(200).json({
-      status: 'Success',
-      data: {
-        user: result
-      }
+    user.getUser(req.body, result => {
+        console.log('RESULT FROM LOGIN RESPONSE: ', result);
+        // res.json(result);
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                user: result
+            }
+        });
     });
-  });
 };
 
 // Add new user to database.
 const signUp = (req, res, next) => {
-  user.addUser(req.body, result => {
-    console.log('RESULT FROM SIGN UP RESPONSE: ', result);
-    // res.json(result);
-    res.status(200).json({
-      status: 'Success',
-      data: {
-        user: result
-      }
+    user.addUser(req.body, result => {
+        console.log('RESULT FROM SIGN UP RESPONSE: ', result);
+        // res.json(result);
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                user: result
+            }
+        });
     });
-  });
 };
 
 // Logout the users
 const logout = (req, res, next) => {
-  // Delete session from the database to logout users.
-  req.session.destroy(error => {
-    console.log(error);
-    // redirect user to the home page after logging out.
-    res.redirect('/');
-  });
+    // Delete session from the database to logout users.
+    req.session.destroy(error => {
+        console.log(error);
+        // redirect user to the home page after logging out.
+        res.redirect('/');
+    });
 };
 
 // Exporting Methods.
 module.exports = {
-  homePage,
-  login,
-  signUp,
-  logout
+    homePage,
+    login,
+    signUp,
+    logout
 };
--- models/User.js
+++ models/User.js
@@ -5,38 +5,40 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 // Create User Schema
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please Tell Us Your Name!']
-  },
-  email: {
-    type: String,
-    required: [true, 'Please Provide Your Email!'],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, 'Please Provide a Valid Email!!']
-  },
-  password: {
-    type: String,
-    required: [true, 'Please Provide a Password!'],
-    minlength: 8
-  },
-  country: {
-    type: String,
-    required: [true, 'Your Country Here!']
-  },
-  imageUrl: {
-    type: String,
-    required: false
-  },
-  movies_list: {
-    watch_list: [],
-    watched_list: []
-  },
-  shows_list: {
-    watch_list: [],
-    watched_list: []
-  }
+    name: {
+        type: String,
+        required: [true, 'Please Tell Us Your Name!'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Please Provide Your Email!'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please Provide a Valid Email!!']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please Provide a Password!'],
+        minlength: 8
+    },
+    country: {
+        type: String,
+        required: [true, 'Your Country Here!'],
+        trim: true
+    },
+    imageUrl: {
+        type: String,
+        required: false
+    },
+    movies_list: {
+        watch_list: [],
+        watched_list: []
+    },
+    shows_list: {
+        watch_list: [],
+        watched_list: []
+    }
 });
 // eslint-disable-next-line new-cap
 const User = new mongoose.model('User', userSchema);
@@ -45,220 +47,220 @@ const User = new mongoose.model('User', userSchema);
 
 // Get one user.
 const getUser = (user, callback) => {
-  User.find(user, (error, data) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(data);
-    }
-  });
+    User.find(user, (error, data) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(data);
+        }
+    });
 };
 
 // Add new user to database.
 const addUser = (newUser, callback) => {
-  User.create(newUser, (error, data) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(data);
-    }
-  });
+    User.create(newUser, (error, data) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(data);
+        }
+    });
 };
 
 // MOVIES FUNCTIONS ****************************************************************************
 
 // Add new movie to watch list.
 const addMovieToWatchList = (userID, newMovie, callback) => {
-  User.findOneAndUpdate(
-    {
-      _id: userID
-    },
-    {
-      $push: {
-        'movies_list.watch_list': newMovie
-      }
-    },
-    (error, data) => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    }
-  );
+    User.findOneAndUpdate(
+        {
+            _id: userID
+        },
+        {
+            $push: {
+                'movies_list.watch_list': newMovie
+            }
+        },
+        (error, data) => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
+        }
+    );
 };
 
 // Delete movie from watch list.
 const deleteMovieFromWatchList = (userID, movieID, callback) => {
-  User.update(
-    { _id: userID },
-    {
-      'movies_list.watch_list': movieID
-    },
-    {
-      $pull: {
-        'movies_list.$.watch_list': {
-          id: movieID
+    User.update(
+        { _id: userID },
+        {
+            'movies_list.watch_list': movieID
+        },
+        {
+            $pull: {
+                'movies_list.$.watch_list': {
+                    id: movieID
+                }
+            }
+        },
+        (error, data) => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
         }
-      }
-    },
-    (error, data) => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    }
-  );
+    );
 };
 
 // Add new movie to watched list.
 const addMovieToWatchedList = (userID, newMovie, callback) => {
-  User.findOneAndUpdate(
-    {
-      _id: userID
-    },
-    {
-      $push: {
-        'movies_list.watched_list': newMovie
-      }
-    },
-    (error, data) => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    }
-  );
+    User.findOneAndUpdate(
+        {
+            _id: userID
+        },
+        {
+            $push: {
+                'movies_list.watched_list': newMovie
+            }
+        },
+        (error, data) => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
+        }
+    );
 };
 
 // Delete movie from watched list.
 const deleteMovieFromWatchedList = (userID, movieID, callback) => {
-  User.update(
-    {
-      _id: userID
-    },
-    {
-      $pull: {
-        'movies_list.watched_list': {
-          id: movieID
-        }
-      }
-    },
-    (error,
-    data => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    })
-  );
+    User.update(
+        {
+            _id: userID
+        },
+        {
+            $pull: {
+                'movies_list.watched_list': {
+                    id: movieID
+                }
+            }
+        },
+        (error,
+        data => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
+        })
+    );
 };
 
 // TV SHOWS FUNCTIONS ****************************************************************************
 
 // Add new show to watch list.
 const addShowToWatchList = (userID, newShow, callback) => {
-  User.findOneAndUpdate(
-    {
-      _id: userID
-    },
-    {
-      $push: {
-        'shows_list.watch_list': newShow
-      }
-    },
-    (error, data) => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    }
-  );
+    User.findOneAndUpdate(
+        {
+            _id: userID
+        },
+        {
+            $push: {
+                'shows_list.watch_list': newShow
+            }
+        },
+        (error, data) => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
+        }
+    );
 };
 
 // Delete show from watch list.
 const deleteShowFromWatchList = (userID, showID, callback) => {
-  User.update(
-    {
-      _id: userID
-    },
-    {
-      $pull: {
-        'shows_list.watch_list': {
-          _id: showID
-        }
-      }
-    },
-    (error,
-    data => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    })
-  );
+    User.update(
+        {
+            _id: userID
+        },
+        {
+            $pull: {
+                'shows_list.watch_list': {
+                    _id: showID
+                }
+            }
+        },
+        (error,
+        data => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
+        })
+    );
 };
 
 // Add new show to watched list.
 const addShowToWatchedList = (userID, newShow, callback) => {
-  User.findOneAndUpdate(
-    {
-      _id: userID
-    },
-    {
-      $push: {
-        'shows_list.watched_list': newShow
-      }
-    },
-    (error, data) => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    }
-  );
+    User.findOneAndUpdate(
+        {
+            _id: userID
+        },
+        {
+            $push: {
+                'shows_list.watched_list': newShow
+            }
+        },
+        (error, data) => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
+        }
+    );
 };
 
 // Delete show from watched list.
 const deleteShowFromWatchedList = (userID, showID, callback) => {
-  User.update(
-    {
-      _id: userID
-    },
-    {
-      $pull: {
-        'shows_list.watched_list': {
-          _id: showID
-        }
-      }
-    },
-    (error,
-    data => {
-      if (error) {
-        callback(error);
-      } else {
-        callback(data);
-      }
-    })
-  );
+    User.update(
+        {
+            _id: userID
+        },
+        {
+            $pull: {
+                'shows_list.watched_list': {
+                    _id: showID
+                }
+            }
+        },
+        (error,
+        data => {
+            if (error) {
+                callback(error);
+            } else {
+                callback(data);
+            }
+        })
+    );
 };
 
 // Exporting Methods.
 module.exports = {
-  getUser,
-  addUser,
-  addMovieToWatchList,
-  deleteMovieFromWatchList,
-  addMovieToWatchedList,
-  deleteMovieFromWatchedList,
-  addShowToWatchList,
-  deleteShowFromWatchList,
-  addShowToWatchedList,
-  deleteShowFromWatchedList
+    getUser,
+    addUser,
+    addMovieToWatchList,
+    deleteMovieFromWatchList,
+    addMovieToWatchedList,
+    deleteMovieFromWatchedList,
+    addShowToWatchList,
+    deleteShowFromWatchList,
+    addShowToWatchedList,
+    deleteShowFromWatchedList
 };
