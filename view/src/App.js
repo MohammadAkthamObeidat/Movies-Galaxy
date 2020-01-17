de385038e86e322c085fa3252a3411cc8bae0eb9
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2020-01-17 14:48:04 +0200

    React Components And Layout Intialized.

--- view/src/App.js
+++ view/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./Assets/CSS/App.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // App State.
+  state = {
+
+  }
+
+  // Rendering 
+  render() {
+    return (
+      <div className="App">
+        Movies Galaxy React App
     </div>
-  );
+    );
+  }
 }
 
 export default App;
